7e26b5fb19d2d680057b6ca19045803f31a944b3
peterpan <apppeterpan@gmail.com>
2022-03-01 15:31:45 +0800

    add SnapshotView preview & shadow

--- CreateAppScreenshot/ScreenshotView.swift
+++ CreateAppScreenshot/ScreenshotView.swift
@@ -12,10 +12,26 @@ struct SnapshotView: View {
     let deviceImage: UIImage
     
     var body: some View {
-        ZStack(alignment: .center) {
+        ZStack {
             Image(uiImage: screenshotImage)
             Image(uiImage: deviceImage)
         }
+        .shadow(radius: 10)
         .ignoresSafeArea()
+    }
+}
+
+
+struct SnapshotView_Previews: PreviewProvider {
+    static var previews: some View {
+        let screenshotImageData = try! Data(contentsOf: Bundle.main.url(forResource: "Simulator Screen Shot - iPhone 13 - 2022-02-28 at 22.26.31.png", withExtension: nil, subdirectory: Directory.screenshot.rawValue)!)
+        let screenshotImage = UIImage(data: screenshotImageData, scale: 3)!
+        let deviceImageData = try! Data(contentsOf: Bundle.main.url(forResource: "iPhone 13 - Starlight.png", withExtension: nil, subdirectory: Directory.device.rawValue)!)
+        let deviceImage = UIImage(data: deviceImageData, scale: 3)!
+        return SnapshotView(screenshotImage: screenshotImage, deviceImage: deviceImage)
+            .scaleEffect(0.8)
+            .previewDevice("iPhone 13")
+        
+        
     }
 }
