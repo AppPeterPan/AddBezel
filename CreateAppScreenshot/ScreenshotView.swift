102c34a6f0f8803bed83008340a8394dda2713ae
rose <rose@gmail.com>
2022-09-09 16:10:27 +0800

    update

--- CreateAppScreenshot/ScreenshotView.swift
+++ CreateAppScreenshot/ScreenshotView.swift
@@ -24,13 +24,13 @@ struct SnapshotView: View {
 
 struct SnapshotView_Previews: PreviewProvider {
     static var previews: some View {
-        let screenshotImageData = try! Data(contentsOf: Bundle.main.url(forResource: "IMG_5256.PNG", withExtension: nil, subdirectory: Directory.screenshot.rawValue)!)
+        let screenshotImageData = try! Data(contentsOf: Bundle.main.url(forResource: "Simulator Screen Shot - iPhone 14 Pro - 2022-09-09 at 16.06.56.png", withExtension: nil, subdirectory: Directory.screenshot.rawValue)!)
         let screenshotImage = UIImage(data: screenshotImageData, scale: 3)!
-        let deviceImageData = try! Data(contentsOf: Bundle.main.url(forResource: "iPhone 11 Pro - Portrait.png", withExtension: nil, subdirectory: Directory.device.rawValue)!)
+        let deviceImageData = try! Data(contentsOf: Bundle.main.url(forResource: "iPhone 14 Pro - Deep Purple - Portrait.png", withExtension: nil, subdirectory: Directory.device.rawValue)!)
         let deviceImage = UIImage(data: deviceImageData, scale: 3)!
         return SnapshotView(screenshotImage: screenshotImage, deviceImage: deviceImage)
             .scaleEffect(0.8)
-            .previewDevice("iPhone 11 Pro")
+            .previewDevice("iPhone 14 Pro")
         
         
     }
